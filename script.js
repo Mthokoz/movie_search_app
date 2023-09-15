c99dda8d36e60ab6e8b238438e735639876283dc
Mthokozeni Sithole <sthmth002@cs.uct.ac.za>
2023-09-15 22:28:40 +0200

    Search function

--- script.js
+++ script.js
@@ -19,14 +19,45 @@ function returnMovies(url){
         console.log(data.results);
         data.results.forEach(elements => {
             const div_card = document.createElement('div');
+            div_card.setAttribute('class','card');
+
             const div_row = document.createElement('div');
+            div_row.setAttribute('class','row');
+
             const div_column = document.createElement('div');
+            div_column.setAttribute('class','column');
+
             const image = document.createElement('img');
+            image.setAttribute('class', 'thumbnail');
+            image.setAttribute('id', 'image');
+
             const title = document.createElement('h3');
+            title.setAttribute('id', 'title');
             const center = document.createElement('center');
 
             title.innerHTML = '${element.title}';
             image.src = IMG_PATH + elements.poster_path;
+            
+            center.appendChild(image);
+            div_card.appendChild(center);
+            div_card.appendChild(title);
+            div_column.appendChild(div_card);
+            div_row.appendChild(div_column);
+
+            main.appendChild(div_row);
+
         });
     });
 }
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    main.innerHTML='';
+
+    const searchItem = search.ariaValueMax;
+
+    if (searchItem){
+        returnMovies(SEARCHAPI + searchitem);
+        search.value = "";
+    }
+});
